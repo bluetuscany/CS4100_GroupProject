58950ff930cd8ff58063b8dde47bd66b1dca6c1d
Ryan Keleti <github@ryankeleti.com>
2021-06-10 20:21:35 -0400

    update params to generator

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
 
 dogs_train = pd.read_csv('labels.csv', names=['id', 'breed'])
 dogs_train['id'] = dogs_train['id'].apply(lambda x : x + '.jpg')
@@ -14,10 +16,12 @@ train_generator = generator.flow_from_dataframe(
     directory='./train/',
     x_col='id',
     y_col='breed',
+    batch_size=32,
     subset='training',
     color_mode='grayscale',
     target=(512, 512),
+    interpolation='nearest',
     save_to_dir='./generated/',
+    save_prefix='',
     save_format='jpg')
-
 
