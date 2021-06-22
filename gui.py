16e0cb31fa1e96742afb928c92094304a828706a
Ryan Keleti <github@ryankeleti.com>
2021-06-22 16:50:20 -0400

    add predict button

--- gui.py
+++ gui.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+SAVED_MODEL_PATH = './saved_model'
 
 class App(tk.Frame):
   def __init__(self, master):
@@ -9,22 +14,35 @@ class App(tk.Frame):
     self.pack()
 
     self.select_image_button = ttk.Button(self, text='Select an image', command=self.select_image)
+    self.predict_button= ttk.Button(self, text='Predict', command=self.predict)
     self.select_image_button.pack()
+    self.predict_button.pack()
+    self.model = keras.models.load_model(SAVED_MODEL_PATH)
 
   def select_image(self):
     filetypes = (
       ('All', '.*'),
       ('JPEG', '.jpg .jpeg'),
       ('PNG', '.png'))
-    filename = filedialog.askopenfilename(
+    self.filename = filedialog.askopenfilename(
       title='Select an image',
-      initialdir='~/',
+      initialdir='./',
       filetypes=filetypes)
 
-    img = Image.open(filename)
+    img = Image.open(self.filename)
     self.img = ImageTk.PhotoImage(img)
     label = ttk.Label(self, image=self.img)
     label.pack()
+
+  def predict(self):
+    if self.filename:
+      img = image.load_img(self.filename, target_size=(128, 128))
+      img = image.img_to_array(img)
+      img = np.expand_dims(img, axis=0)
+      result = self.model.predict_classes(img)
+      print(result)
+    else:
+      print('none selected')
 
 WIDTH = 1024
 HEIGHT = 512
